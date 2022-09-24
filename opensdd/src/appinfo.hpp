553d8bb76765a1a98f98dd09f975f1edd43cf590
seek <seek@ntr0py.org>
2022-09-23 20:12:38 -0700

    Build:     Cleaned up CMakeLists a little
    opensdd:
        Added support for a few basic program arguments
        Added application info

---
+++ opensdd/src/appinfo.hpp
@@ -0,0 +1,87 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//  OpenSD
+//  An open-source userspace driver for Valve's Steam Deck hardware
+//
+//  Copyright 2022 seek
+//  https://gitlab.com/seek-dev/opensd
+//  Licensed under the GNU GPLv3+
+//
+//    This program is free software: you can redistribute it and/or modify it under the terms of 
+//    the GNU General Public License as published by the Free Software Foundation, either 
+//    version 3 of the License, or (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+//    without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+//    See the GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License along with this program. 
+//    If not, see <https://www.gnu.org/licenses/>.             
+////////////////////////////////////////////////////////////////////////////////////////////////////
+#ifndef __APPINFO_HPP__
+#define __APPINFO_HPP__
+
+#include <string>
+
+
+namespace AppInfo
+{
+    const std::string   PROJECT         = "OpenSD";
+    const std::string   PROJECT_PAGE    = "https://gitlab.com/seek-dev/opensd";
+    const std::string   APP_NAME        = "OpenSD daemon";
+    const std::string   APP_SHORT_NAME  = "opensdd";
+    const std::string   AUTHOR          = "seek";
+    const std::string   COPYRIGHT_YEAR  = "2022";
+    const std::string   MAJOR_VER       = "0";
+    const std::string   MINOR_VER       = "21";
+    const std::string   SPDX_LICENSE    = "GPL-3.0-or-later";
+    const std::string   INFO_BLOCK      =
+    {
+        APP_NAME + "\n" +
+        "Copyright " + COPYRIGHT_YEAR + " " + AUTHOR + "\n" +
+        "Version " + MAJOR_VER + "." + MINOR_VER + "\n" +
+        PROJECT_PAGE + "\n" +
+        "Licensed under the GNU GPLv3+\n"
+    };
+    const std::string   LICENSE_BLOCK   =
+    {
+        "    This program comes with ABSOLUTELY NO WARRANTY.\n"
+        "    This is free software, and you are welcome to redistribute it under certain\n"
+        "    conditions.  See https://gitlab.com/seek-dev/opensd for license.\n"
+    };
+    const std::string   HELP_BLOCK      =
+    {
+        "  Usage:\n"
+        "\n"
+        "    -h    --help             Show this help message.\n"
+        "    -v    --version          Output version number and exit.\n"
+        "    -l    --log-level        Set minumum logging level.  Default: 'warn'\n"
+        "                             Valid options are:\n"
+        "                                 verbose, debug, info, warn, error\n"
+    };
+} // namespace AppInfo
+
+
+// Prototype CLI
+/*
+OpenSD Daemon
+Copyright 2022 seek
+Version 0.000
+Licensed under the GNU GPLv3+
+    
+    This program comes with ABSOLUTELY NO WARRANTY. 
+    This is free software, and you are welcome to redistribute it under certain
+    conditions.  See https://gitlab.com/seek-dev/opensd for license.
+
+  Usage:
+    
+    -h      --help              Show this help message.
+    -v      --version           Output version number and exit.
+            --log-level         Set minumum logging level.  Default: 'warn'
+                                Valid options are:
+                                    verbose, debug, info, warn, error
+*/
+
+
+
+
+#endif // __APPINFO_HPP__
--- opensdd/src/main.cpp
+++ opensdd/src/main.cpp
@@ -19,14 +19,116 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 #include "log.hpp"
 #include "daemon.hpp"
+#include "prog_args.hpp"
+#include "appinfo.hpp"
+#include <vector>
+#include <string>
+#include <iostream>
 
 
-int main()
+void ShowInfoLine()
 {
+    std::cout << AppInfo::INFO_BLOCK << std::endl;
+    std::cout << AppInfo::LICENSE_BLOCK << std::endl;
+}
+
+
+
+void ShowHelp()
+{
+    std::cout << AppInfo::HELP_BLOCK << std::endl;
+}
+
+
+
+void ShowVersion()
+{
+    std::cout << AppInfo::MAJOR_VER << "." << AppInfo::MINOR_VER << std::endl;
+}
+
+
+
+int main( int argc, char **argv )
+{
+    std::vector<std::string>    arg_list( argv, argv + argc );
+    Daemon                      opensdd;
+    ProgArgs                    args( arg_list );
+    
+    // Set initial Logging level
     gLog.SetFilterLevel( Log::VERB );
 
-    Daemon          opensdd;
+    // Version
+    if (args.HasOpt( "v", "version" ))
+    {
+        ShowVersion();
+        return 0;
+    }
+    
+    ShowInfoLine();
+    
+    // CLI help
+    if (args.HasOpt( "h", "help" ))
+    {
+        ShowHelp();
+        return 0;
+    }
+    
+    // Logging level
+    if (args.HasOpt( "l", "log-level" ))
+    {
+        std::vector<std::string>    levels = { "", "verb", "verbose", "debug", "info", "warn", "warning", "error" }; 
+        std::string                 result;
+        unsigned int                i = 0;
+        
+        result = args.GetOptParam( "l", "log-level" );
+        for (auto& s : levels)
+        {
+            if (s == result)
+                break;
+            ++i;
+        }
+        
+        switch (i)
+        {
+            case 1: // verb
+            case 2: // verbose
+                gLog.SetFilterLevel( Log::VERB );
+            break;
+            
+            case 3: // sebug
+                gLog.SetFilterLevel( Log::DEBUG);
+            break;
+            
+            case 4: // info
+                gLog.SetFilterLevel( Log::INFO );
+            break;
+            
+            case 5: // warn
+            case 6: // warning
+                gLog.SetFilterLevel( Log::WARN );
+            break;
+            
+            case 7: // error
+                gLog.SetFilterLevel( Log::ERROR );
+            break;
+            
+            case 0: // nada
+            default:  // Unknown
+                std::cout << "Invalid option.  Run again with --help for usage.\n";
+                return -1;
+            break;
+        }
+    }
     
     
+    // Exit if there were argument parsing errors
+    if (args.GetErrorCount())
+    {
+        std::cout << "Invalid option.  Run again with --help for usage.\n";
+        return -1;
+    }
+    
+    
+    // Run daemon and exit
     return opensdd.Run();
 }
